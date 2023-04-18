f2a2485924a7bbbfce08e896fe29abc7210e4167
Amanda Hernández <avhernan@terpmail.umd.edu>
2023-04-18 19:40:09 -0400

    finished lab exercise

--- javascript.js
+++ javascript.js
@@ -7,39 +7,39 @@ var barChart = new Chart(ctx, {
         datasets: [{
             label: "America", 
             data: [18791.13],
-            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-            borderColor: ['rgb(255, 99, 132)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(186, 133, 171, 0.2)'],
+            borderColor: ['rgb(186, 133, 171)'],
+            borderWidth: 2,
         },{
             label: "East Asia & Pacific", 
             data: [14480.30],
-            backgroundColor: ['rgba(255, 159, 64, 0.2)'],
-            borderColor: ['rgb(255, 159, 64)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(94, 99, 164, 0.2)'],
+            borderColor: ['rgb(94, 99, 164)'],
+            borderWidth: 2,
         },{
             label: "Europe & Central Asia", 
             data: [20818.45],
-            backgroundColor: ['rgba(255, 205, 86, 0.2)'],
-            borderColor: ['rgb(255, 205, 86)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(0, 59, 94, 0.2)'],
+            borderColor: ['rgb(0, 59, 94)'],
+            borderWidth: 2,
         },{
             label: "Middle East & North Africa", 
             data: [13856.00],
-            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
-            borderColor: ['rgb(75, 192, 192)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(61, 106, 151, 0.2)'],
+            borderColor: ['rgb(61, 106, 151)'],
+            borderWidth: 2,
         },{
             label: "South Asia", 
             data: [2505.17],
-            backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-            borderColor: ['rgb(54, 162, 235)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(83, 141, 202, 0.2)'],
+            borderColor: ['rgb(83, 141, 202)'],
+            borderWidth: 2,
         },{
             label: "Sub-Saharan Africa", 
             data: [2569.00],
-            backgroundColor: ['rgba(153, 102, 255, 0.2)'],
-            borderColor: ['rgb(153, 102, 255)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(252, 185, 88, 0.2)'],
+            borderColor: ['rgb(252, 185, 88)'],
+            borderWidth: 2,
         }]
     },
     options: {
@@ -59,7 +59,11 @@ var barChart = new Chart(ctx, {
         },
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'GDP per Capita'
+                }
             }
         },
     },
@@ -74,33 +78,33 @@ var lineChart = new Chart(ctx, {
         datasets: [{
             label: "Brazil", 
             data: [32,32,31.99,32.06,32.4,32.75,51.78,63.65,71.44,76.33],
-            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-            borderColor: ['rgb(255, 99, 132)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(186, 133, 171, 0.2)'],
+            borderColor: ['rgb(186, 133, 171)'],
+            borderWidth: 2,
         },{
             label: "China", 
             data: [32,32,32,31.96,31.75,31.55,40.73,63.26,70.03,77.4],
-            backgroundColor: ['rgba(255, 159, 64, 0.2)'],
-            borderColor: ['rgb(255, 159, 64)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(94, 99, 164, 0.2)'],
+            borderColor: ['rgb(94, 99, 164)'],
+            borderWidth: 2,
         },{
             label: "India", 
             data: [25.44,25.44,25.44,25.11,18.37,26.69,34.92,51.51,62.34,69.77],
-            backgroundColor: ['rgba(255, 205, 86, 0.2)'],
-            borderColor: ['rgb(255, 205, 86)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(0, 59, 94, 0.2)'],
+            borderColor: ['rgb(0, 59, 94)'],
+            borderWidth: 2,
         },{
             label: "Indonesia", 
             data: [30,30,30,30.04,30.24,30.43,37.16,56.49,67.27,72.51],
-            backgroundColor: ['rgba(75, 192, 192, 0.2)'],
-            borderColor: ['rgb(75, 192, 192)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(61, 106, 151, 0.2)'],
+            borderColor: ['rgb(61, 106, 151)'],
+            borderWidth: 2,
         },{
             label: "United States", 
             data: [39.41,39.41,39.41,39.41,48.92,58.47,68.07,72.44,76.9,79.59],
-            backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-            borderColor: ['rgb(54, 162, 235)'],
-            borderWidth: 1,
+            backgroundColor: ['rgba(83, 141, 202, 0.2)'],
+            borderColor: ['rgb(83, 141, 202)'],
+            borderWidth: 2,
         },
     ]
     },
@@ -121,11 +125,126 @@ var lineChart = new Chart(ctx, {
         },
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Life Expectancy'
+                }
             }
         },
     },
 });
 
 // scatterplot
-
+var ctx = document.getElementById('scatterChart').getContext('2d');
+      var scatterChart = new Chart(ctx, {
+          type: 'scatter',
+          data: {
+            datasets: [{ 
+                data: [
+                    {x: 71.5, y: 3678},
+                ],
+                label: "China",
+                backgroundColor: ['rgba(61, 106, 151, 0.2)'],
+                borderColor: ['rgb(61, 106, 151)'],
+                borderWidth:2,
+               
+              },{ 
+                data: [
+                    {x: 61.1, y: 2548},
+                ],
+                label: "India",
+                backgroundColor: ['rgba(83, 141, 202, 0.2)'],
+                borderColor: ['rgb(83, 141, 202)'],
+                borderWidth:2,
+                
+              },{ 
+                data: [
+                    {x: 77.1, y: 45986},
+                ],
+                label: "United States",
+                backgroundColor:"rgb(180, 82, 72,0.2)",
+                borderColor: ['rgb(180, 82, 72)'],
+                borderWidth:2,
+              },{
+                data: [
+                    {x: 68.3, y: 5878},
+                ],
+                label: "Indonesia",
+                backgroundColor:"rgb(212, 140, 132,0.2)",
+                borderColor: ['rgb(212, 140, 132)'],
+                borderWidth:2,
+              },{
+                data: [
+                    {x: 71.9, y: 11461},
+                ],
+                label: "Brazil",
+                backgroundColor:"rgb(168, 154, 73,0.2)",
+                borderColor: ['rgb(168, 154, 73)'],
+                borderWidth:2,
+              },{
+                data: [
+                    {x: 65.4, y: 13173},
+                ],
+                label: "Russia",
+                backgroundColor:"rgb(214, 207, 162,0.2)",
+                borderColor: ['rgb(214, 207, 162)'],
+                borderWidth:2,
+              },{
+                data: [
+                    {x: 62.6, y: 3366},
+                ],
+                label: "Pakistan",
+                backgroundColor:"rgb(60, 180, 100,0.2)",
+                borderColor: ['rgb(60, 180, 100)'],
+                borderWidth:2,
+              },{
+                data: [
+                    {x: 65.8, y: 1632},
+                ],
+                label: "Bangladesh",
+                backgroundColor: ['rgba(186, 133, 171, 0.2)'],
+                borderColor: ['rgb(186, 133, 171)'],
+                borderWidth:2,
+              },{
+                data: [
+                    {x: 81.1, y: 32193},
+                ],
+                label: "Japan",
+                backgroundColor: ['rgba(94, 99, 164, 0.2)'],
+                borderColor: ['rgb(94, 99, 164)'],
+                borderWidth:2,
+              }
+            ]
+          },
+          options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'The relationship between GDP per capita and life expectancy',
+                    font: {
+                        size: 18
+                    }
+                },
+                subtitle: {
+                    display: true,
+                    text: 'Countries with higher life expectancies tend to also have higher GDPs per capita.'
+                }
+            },
+            scales: {
+              y: {
+                title: {
+                  display: true,
+                  text: 'GDP per Capita'
+                }
+              },
+              x: {
+                  title: {
+                      display: true,
+                      text: 'Life Expectancy'
+                  }
+              }
+            }
+          }
+        });
